f2edca89ff604a5469713fc4585743db481f0223
Xiao-Fan-x <68093976+Xiao-Fan-x@users.noreply.github.com>
2021-02-19 13:02:05 +0800

    HelloThread change

--- day01/src/atguigu/exer/ThreadDemo.java
+++ day01/src/atguigu/exer/ThreadDemo.java
@@ -9,37 +9,37 @@ package atguigu.exer;
  */
 public class ThreadDemo {
     public static void main(String[] args) {
-//        MyThread1 m1 = new MyThread1();
-//        MyThread2 m2 = new MyThread2();
-//
-//        m1.start();
-//        m2.start();
+        Thread m1 = new MyThread1();
+        Thread m2 = new MyThread2();
+
+        m1.start();
+        m2.start();
 
         //创建Thread类的匿名子类的方式
-        new Thread(){
-            @Override
-            public void run() {
-                for (int i = 0; i < 100; i++) {
-                    if(i % 2 == 0){
-                        System.out.println(Thread.currentThread().getName() + ":" + i);
-
-                    }
-                }
-            }
-        }.start();
-
-
-        new Thread(){
-            @Override
-            public void run() {
-                for (int i = 0; i < 100; i++) {
-                    if(i % 2 != 0){
-                        System.out.println(Thread.currentThread().getName() + ":" + i);
-
-                    }
-                }
-            }
-        }.start();
+//        new Thread(){
+//            @Override
+//            public void run() {
+//                for (int i = 0; i < 100; i++) {
+//                    if(i % 2 == 0){
+//                        System.out.println(Thread.currentThread().getName() + ":" + i);
+//
+//                    }
+//                }
+//            }
+//        }.start();
+//
+//
+//        new Thread(){
+//            @Override
+//            public void run() {
+//                for (int i = 0; i < 100; i++) {
+//                    if(i % 2 != 0){
+//                        System.out.println(Thread.currentThread().getName() + ":" + i);
+//
+//                    }
+//                }
+//            }
+//        }.start();
 
     }
 }
